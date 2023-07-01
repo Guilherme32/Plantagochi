727f54d3b057bb5fe4c0d7380b50ac182d6c6b44
Guilherme <guilherme_albuquerque2@hotmail.com>
2023-07-01 19:22:52 -0300

    Adding sensor status led
    This commit has some tests with a breathing led that doesn't have any discontinuities when the period changes

--- Firmware/main/main.c
+++ Firmware/main/main.c
@@ -9,14 +9,29 @@
 #include <inttypes.h>
 #include "esp_err.h"
 #include "esp_intr_alloc.h"
+#include "esp_wifi_types.h"
+#include "hal/ledc_types.h"
 #include "portmacro.h"
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #include "dht20.h"
+#include "soc/clk_tree_defs.h"
 #include "soil_humidity.h"
 #include "wifi.h"
+
+#include <math.h>
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+
+#define PI 3.14
+#define DELTA_T (1000 / (double) configTICK_RATE_HZ)
+#define LEDC_MODE LEDC_LOW_SPEED_MODE
+#define LEDC_CHANNEL LEDC_CHANNEL_0
+
+#define WIFI_LED_PIN 27
+#define OUTPUT_PINS_MASK ((1ULL << WIFI_LED_PIN))
 
 
 void test_dth20_task() {
@@ -57,12 +72,46 @@ void app_main(void)
     ESP_ERROR_CHECK(moisture_sensor_init());
     ESP_ERROR_CHECK(wifi_init());
 
-    // xTaskCreate(
-    //     test_moisture_sensor_task,
-    //     "Moisture test",
-    //     4096,
-    //     NULL,
-    //     10,
-    //     NULL
-    //     );
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode = LEDC_MODE,
+        .timer_num = LEDC_TIMER_0,
+        .duty_resolution = LEDC_TIMER_10_BIT,
+        .freq_hz = 5000,
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode = LEDC_MODE,
+        .channel = LEDC_CHANNEL,
+        .timer_sel = LEDC_TIMER_0,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = WIFI_LED_PIN,
+        .duty = 0,
+        .hpoint = 0
+    };
+
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    double phase = 0;
+    while (1) {
+        for(int i=1; i<10; i++) {
+            double period = i * 1000;
+
+            for (int j=0; j<31490/portTICK_PERIOD_MS; j++) {
+                phase += 2 * PI * DELTA_T / period;
+                // printf("Time: %f \n", time);
+                while (phase > 2 * PI) {
+                    phase -= 2 * PI;
+                }
+                int new_duty = (int) (1024 * powf((sin(phase)), 2));
+
+                ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, new_duty));
+                ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+
+                vTaskDelay(1);
+            }
+        }
+    }
 }
